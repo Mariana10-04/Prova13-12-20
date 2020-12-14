712c3d9f9461216997e151b2327194b2d9e29806
Mariana10-04 <71359480+Mariana10-04@users.noreply.github.com>
2020-12-14 18:25:17 -0300

    cadastro

--- src/components/pages/cadastro/Cadastro.js
+++ src/components/pages/cadastro/Cadastro.js
@@ -1,72 +1,53 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React from 'react'
 import './cadastro.css'
 
-export default function AdminPage() {
-  const history = useHistory();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [check, setCheck] = useState("");
-
-  function handleSubmit(event) {
-    const loginData = {
-      email: email,
-      password: password,
-    };
-
-    event.preventDefault();
+function msg() {
+    alert("Agora foi");
   }
 
-  function handleEmail(e) {
-    setEmail(e.target.value);
-    console.log(e.target.value);
-  }
-
-  function handlePassword(e) {
-    setPassword(e.target.value);
-    console.log(e.target.value);
-  }
-
-  return (
+function cadastro() {
+    return(
     <>
-      <div className="container-new-post">
+       <form action="/" method="post" id="casaDasto">
 
-        <br />
-        <form onSubmit={handleSubmit}>
-          <div className="inputs">
-            <input
-              type="email"
-              name="admin_email"
-              id="admin_email"
-              placeholder="Insira seu Email"
-              onChange={handleEmail}
-              required
-              value={email}
-            />
-            <br />
-            <input
-              type="password"
-              name="admin_password"
-              id="admin_password"
-              placeholder="Insira o Password"
-              onChange={handlePassword}
-              required
-              value={password}
-            />
-          </div>
-          <br />
-          <br />
+            <div>
+                <label>
+                    Nome: 
+                    <input type="text" />
+                </label>
+            </div>
 
-          <input type="submit" className="button button1" value="LOGIN" />
-        </form>
+            <div>
+				<label>
+                    Email: 
+                    <input type="text" />
+                </label>
+            </div>
 
-        {check ? (
-          <p style={{ color: "red" }}>
-            Email: admin@admin <br />
-            Senha: admin
-          </p>
-        ) : null}
-      </div>
+            <div>
+				<label>
+                    Tel: 
+                    <input type="text" />
+                </label>
+            </div>
+
+            <div>
+				<label>
+                    Data de nascimento: 
+                    <input type="text"/>
+                </label>
+            </div>
+
+            <div>
+
+            <button onClick={msg}>
+                Inscrever-se
+            </button>
+
+            </div>
+            
+</form>
     </>
-  );
+    );
 }
+export default cadastro;
